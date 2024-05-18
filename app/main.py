b983576ab0049ab4b91ca9a45ebc8a6335370823
Shubham9280 <shubhammurkute80@gmail.com>
2024-05-18 20:20:12 +0530

    All code changes done regarding Deployement

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+import json
+
+
+with open("../model/model.pkl","rb") as file:
+    model=pickle.load(file)
+with open("../config/label_mapping.json","rb") as j_file :
+    model_prediction_map=json.load(j_file)
+    
+    
+    
+    
+app=FastAPI()
+
+class Review(BaseModel):
+    text:str
+    
+@app.post("/predict/")
+def predict(review:Review):
+    model_prediction=model.predict(Review.text)
+    return model_prediction_map[str(model_prediction[0])]
+
+if __name__ =="__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port=8000)
